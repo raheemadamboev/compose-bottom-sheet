f078e3db43f41e17ca75e65b3b8e344de19389f4
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-06 18:19:43 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/composebottomsheet/MainActivity.kt
@@ -0,0 +1,26 @@
+package xyz.teamgravity.composebottomsheet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import xyz.teamgravity.composebottomsheet.ui.theme.ComposeBottomSheetTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeBottomSheetTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
