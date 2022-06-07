085cf063d6c6ae26284d2aae6ea921c1af5f2364
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-07 12:58:11 +0500

    add bottom sheet

--- app/src/main/java/xyz/teamgravity/composebottomsheet/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/composebottomsheet/presentation/activity/Main.kt
@@ -3,10 +3,18 @@ package xyz.teamgravity.composebottomsheet.presentation.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import xyz.teamgravity.composebottomsheet.R
 import xyz.teamgravity.composebottomsheet.presentation.theme.ComposeBottomSheetTheme
 
 class Main : ComponentActivity() {
@@ -18,7 +26,75 @@ class Main : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    val bottomSheet = rememberBottomSheetState(initialValue = BottomSheetValue.Collapsed)
+                    val scaffold = rememberBottomSheetScaffoldState(bottomSheetState = bottomSheet)
+                    val scope = rememberCoroutineScope()
 
+                    BottomSheetScaffold(
+                        sheetContent = {
+                            Box(
+                                contentAlignment = Alignment.Center,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(128.dp)
+                            ) {
+                                Text(
+                                    text = stringResource(id = R.string.swipe_up_to_expand),
+                                    color = Color.White
+                                )
+                            }
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(64.dp)
+                            ) {
+                                Text(
+                                    text = stringResource(id = R.string.sheet_content),
+                                    color = Color.White
+                                )
+                                Spacer(modifier = Modifier.height(20.dp))
+                                Button(
+                                    onClick = {
+                                        scope.launch { bottomSheet.collapse() }
+                                    }
+                                ) {
+                                    Text(text = stringResource(id = R.string.click_collapse_sheet))
+                                }
+                            }
+                        },
+                        sheetBackgroundColor = Color.Gray,
+                        scaffoldState = scaffold,
+                        floatingActionButton = {
+                            FloatingActionButton(
+                                onClick = { },
+                                backgroundColor = MaterialTheme.colors.primary
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Add,
+                                    contentDescription = stringResource(id = R.string.cd_add_button)
+                                )
+                            }
+                        },
+                        floatingActionButtonPosition = FabPosition.End,
+                        sheetPeekHeight = 128.dp
+                    ) { padding ->
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(padding)
+                        ) {
+                            Button(
+                                onClick = {
+                                    scope.launch { bottomSheet.expand() }
+                                },
+                                modifier = Modifier.padding(padding)
+                            ) {
+                                Text(text = stringResource(id = R.string.click_expand_sheet) + " ${bottomSheet.progress.fraction}")
+                            }
+                        }
+                    }
                 }
             }
         }
